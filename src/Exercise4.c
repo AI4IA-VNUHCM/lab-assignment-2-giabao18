411edd863f57f893aa06a1ad5d65bc8d4672f242
giabao18 <phamgiabao1862001@gmail.com>
2021-10-11 16:21:01 +0700

    Ex4

--- src/Exercise4.c
+++ src/Exercise4.c
@@ -7,21 +7,53 @@ Ex:
 | Output: 2 7 8 9 5 2 1 0            |
 |____________________________________|
 */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-int main(int argc, char *argv[]) {
+void Increse(int A[], int n)
+{
+	for (int i = 0; i < n / 2 - 1; i++)
+		for (int j = i + 1; j < n / 2; j++)
+			if (A[i] > A[j])
+			{
+				int temp;
+				temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}
+}
+void Decrese(int A[], int n)
+{
+	for (int i = n / 2; i < n - 1; i++)
+		for (int j = i + 1; j < n; j++)
+			if (A[i] < A[j])
+			{
+				int temp;
+				temp = A[i];
+				A[i] = A[j];
+				A[j] = temp;
+			}
+}
+int main(int argc, char *argv[])
+{
 	//testing variable, applying it to your algorithm for auto-evaluating
 	argc--;
 	int n = argc;
-	int test_array[n],i;
-	for(i=0; i<n;i++){
-		test_array[i] = atoi(argv[i+1]);
+	int test_array[n], i;
+	for (i = 0; i < n; i++)
+	{
+		test_array[i] = atoi(argv[i + 1]);
 	}
 	//Your codes here
-	
-	
+	Increse(test_array, n);
+	for (int i = 0; i < n / 2; i++)
+	{
+		printf(" %d", test_array[i]);
+	}
+	Decrese(test_array, n);
+	for (int i = n / 2; i < n; i++)
+	{
+		printf(" %d", test_array[i]);
+	}
 	return 0;
 }

4b3ce9cb19168607f2bd87b9a74512657fcd9d28
giabao18 <phamgiabao1862001@gmail.com>
2021-10-11 13:38:44 +0700

    Ex1

--- src/Exercise1.c
+++ src/Exercise1.c
@@ -18,8 +18,18 @@ int main(int argc, char *argv[]) {
 	//testing variable, applying it to your algorithm for auto-evaluating
 	float distance = atof(argv[1]);
 	int result;
-	//Your codes here
-	
+	if (distance<=2)
+	{
+		result = 15000;
+	}
+	if (distance>2&&distance<=30)
+	{
+		result = 15000 + (distance-2) / 0.25 * 2000;
+		if (distance>30)
+		{
+			result += (distance - 30) * 5000;
+		}
+	}
 	printf("%d", result);
 	return 0;
 }
